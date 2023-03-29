9df5abfd325ac6925a90d64fedd7916b38ca9c08
rubek <rubek@ottr.com.np>
2023-03-29 15:57:44 +0545

    ✅ Test: setup ava testing

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
-
-require("dotenv").config();
+import express, { json } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.get("/", (req, res) => {
   res.json({ message: "👋🌎🌍🌏" });
 });
 
-module.exports = app;
+export default app;
