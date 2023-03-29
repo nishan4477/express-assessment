54ef15a8fc88d04a299b33848f7dec236a93feac
rubek <rubek@ottr.com.np>
2023-03-29 10:31:05 +0545

    🎉 Init: initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "👋🌎🌍🌏" });
+});
+
+module.exports = app;
